2667cc9bd7e5f1fe1fa480a4461e627d88a4f678
Zak Tonks <zaktonks@gmail.com>
2021-12-01 20:01:29 +0000

    Solution for part 1

--- day1/src/main.c
+++ day1/src/main.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
 
-void main()
+int main()
 {
-    
+    FILE* inputptr = fopen("/home/zak/AOC2021/day1/input", "r");
+
+    if (inputptr == NULL)
+        return -1;
+
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    int lastDepth = INT_MAX;
+    int numTimesIncreased = 0;
+
+    while ((read = getline(&line, &len, inputptr)) != -1) {
+
+        int depth = atoi(line);
+
+        numTimesIncreased += depth > lastDepth;
+
+        lastDepth = depth;
+    }
+
+    printf("%d\n", numTimesIncreased);
+
+    return 0;
 }
